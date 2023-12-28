21fdfdb1ce71724eeebce9458131950ca5131668
Jackson Sui <jsui@google.com>
2023-12-28 11:18:00 -0800

    Adds an iOS GitHub actions test.
    PiperOrigin-RevId: 594287476

--- .github/workflows/build/ios_build.sh
+++ .github/workflows/build/ios_build.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-
-set -eo pipefail
-projects=("${REGEX}")
-
-for PROJ_DIR in "${projects[@]}"
-do
-  CHANGES="$(git --no-pager diff --name-only "${COMMIT_RANGE}")";
-  echo "Commit range: ${COMMIT_RANGE}; Project dir: ${PROJ_DIR};\n Changes: ${CHANGES}";
-  if [[ -n "$(grep -E "(${PROJ_DIR}|\.github\/workflows)" <<< "${CHANGES}")" ]]; then
-    echo "Building for ${PROJ_DIR}";
-    pushd "$PROJ_DIR";
-    ./gradlew build;
-    popd;
-  fi
-done
-
-#!/bin/bash
 # Script to build iOS sample app (Objective-C or Swift)
-# $REGEX variable, using build.sh script.
+# $REGEX variable, using ios_build.sh script.
 
 set -eo pipefail
 
