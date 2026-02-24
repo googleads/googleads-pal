7c68f5119fac7ff912ec49aa5e0297769cbb838c
Interactive Media Ads Developer Relations <ima-devrel@google.com>
2026-02-24 11:04:19 -0800

    PAL tvOS sample app
    PiperOrigin-RevId: 874699715

---
+++ tvos/SimpleExample/ViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+/**
+ * A simple view controller that displays a navigation bar with 2 buttons to:
+ *   1. Request, load, and display a nonce (or error) with the PAL SDK.
+ *   2. Reset the nonce loader being used.
+ */
+@interface ViewController : UIViewController
+@end
